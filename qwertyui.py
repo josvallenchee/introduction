addb3d8c3a29f1561cffd4320a7740b13f1f1fcc
ComDisc <jose.vallenchee11@gmail.com>
2017-09-19 22:04:07 +0700

    testing

---
+++ qwertyui.py
@@ -0,0 +1,19 @@
+spacel = 10
+spacer = 10
+x=1
+
+while x<15:
+    print (' '*spacel,'*'*x,' '*spacer)
+    x+=2
+    spacel-=1
+    spacer-=1
+
+spacel2 = 5
+spacer2 = 1
+y=11
+
+while y>0:
+    print (' '*spacel2,'*'*y,' '*spacer2)
+    y-=2
+    spacel2+=1
+    spacer2+=1
